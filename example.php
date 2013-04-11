ddb257ad8094b0ba93d15300c8940cfa158769d9
Azwan <azwan082@gmail.com>
2013-04-11 17:50:03 +0800

    update example

--- example.php
+++ example.php
@@ -1,15 +1,15 @@
 <?php
 $title = 'Hello';
 $message = 'Hello world message';
+$channel = 'notification';
 
 require 'ACSPushNotification.php';
 $apn = new ACSPushNotification(array(
 	'appKey' => '<APP KEY>',
 	'adminName' => '<ADMIN USER NAME>',
-	'adminPass' => '<ADMIN USER PASSWORD>',
-	'channel' => 'notification'
+	'adminPass' => '<ADMIN USER PASSWORD>'
 ));
-$apn->send($title, $message);
+$apn->send($title, $message, $channel);
 var_dump($apn->getLog());
 $apn->close();
 ?>
